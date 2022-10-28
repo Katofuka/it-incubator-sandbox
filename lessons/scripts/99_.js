e89831e83331956d50dd5da87644437b7009c124
katofuka <katopuk@yandex.ru>
2022-10-28 11:33:53 +0700

    kata

--- lessons/scripts/99_.js
+++ lessons/scripts/99_.js
@@ -1,5 +1,34 @@
-function disemvowel(str) {
-    console.log(str.replace(/[aeiouy]/ig, ''));
-    return str.replace(/[aeiouy]/ig, '');
-  }
-  disemvowel("No offense but,\nYour writing is among the worst I've ever read")
+function getCount(str) {
+    //Возвращает количество (количество) гласных в заданной строке.
+    //Мы будем рассматривать a, e, i, o, uкак гласные для этой Ката (но не y).
+    //Входная строка будет состоять только из строчных букв и/или пробелов.
+
+    const countStart = str.length;
+    const newString = str.replace(/[aeiou]/gi, '');
+    return countStart - newString.length;
+}
+
+console.log(getCount("abracadabra"));
+
+function parse(data) {
+    let val = 0
+    const arrayStr = data.split('');
+    let returnArray = [];
+
+    for (let i = 0; i < arrayStr.length; i++) {
+        if (arrayStr[i] === "i") {
+            val++;
+
+        } else if (arrayStr[i] === "d") {
+            val--;
+        } else if (arrayStr[i] === "s") {
+            val = Math.pow(val,2);
+        } else if (arrayStr[i] === "o") {
+            returnArray.push(val);
+        }
+    }
+    return returnArray;
+}
+
+const rend = parse("iiisdoso");
+console.log(rend);
