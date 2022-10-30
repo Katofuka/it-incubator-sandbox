6650e6ae8d0bb40cd82e6cf244f572545454c2ec
katofuka <ladno4ka@gmail.com>
2022-10-30 20:42:00 +0700

    chair

--- lessons/scripts/99_.js
+++ lessons/scripts/99_.js
@@ -51,3 +51,37 @@ function gap(g, m, n) {
     return null;
 }
 
+function meeting(x, need) {
+    let chairArray = [];
+    if (need === 0) return "Game On"
+    else {
+        for (let i = 0; i < x.length; i++) {
+            let tookInRoom = 0;
+
+            let chairOccupied = x[i][0].split('');
+
+            if (chairOccupied.length < x[i][1]) {
+                const free = x[i][1] - chairOccupied.length;
+                if(free <= need) {
+                    tookInRoom += free;                 
+                } else {
+                    tookInRoom += need;
+                }
+                need -= tookInRoom;
+                chairArray.push(tookInRoom);
+
+            } else {
+                chairArray.push(0);
+            }
+            if(need === 0) return chairArray;
+        }
+        if(need > 0 ) return "Not enough!"
+        
+        return chairArray;
+        
+    }
+}
+
+console.log(meeting([['XXX', 3], ['XXXXX', 6], ['XXXXXX', 9]], 4)); //[0, 1, 3]
+console.log(meeting([['XX', 2], ['XXXX', 6], ['XXXXX', 4]], 0));  //'Game On'
+console.log(meeting([['XXX', 1], ['XXXXXX', 6], ['X', 2], ['XXXXXX', 8], ['X', 3], ['XXX', 1]], 5));   // [0, 0, 1, 2, 2]);
